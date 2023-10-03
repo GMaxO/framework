a085cbfec69c6667c66279a814c3dd884c22387f
Maxim Grishko <105946894+GMaxO@users.noreply.github.com>
2023-10-03 10:56:10 +0300

    Delete src/main/java/studies/spring directory

--- src/main/java/studies/spring/homeworkone/Main.java
+++
@@ -1,8 +0,0 @@
-package studies.spring.homeworkone;
-
-public class Main {
-    public static void main(String[] args) {
-        Server server = new Server(64);
-        server.start(9999);
-    }
-}
