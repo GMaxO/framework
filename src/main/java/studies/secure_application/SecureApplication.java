8ad925d36addeceb3eaefb45b2ffbb4e8796c62a
Maxim Grishko <10grishko.mo@gmail.com>
2024-01-13 14:16:43 +0300

    secure_application

---
+++ src/main/java/studies/secure_application/SecureApplication.java
@@ -0,0 +1,13 @@
+package studies.secure_application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecureApplication.class, args);
+    }
+
+}
---
+++ src/main/java/studies/secure_application/service/LibraryApplicationService.java
@@ -0,0 +1,35 @@
+package studies.secure_application.service;
+
+import studies.secure_application.repository.LibraryApplicationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LibraryApplicationService {
+
+    private final LibraryApplicationRepository repository;
+
+    @Autowired
+    public LibraryApplicationService(LibraryApplicationRepository repository) {
+        this.repository = repository;
+    }
+
+    public String hello() {
+        return repository.hello();
+    }
+    public String welcome() {
+        return repository.welcome();
+    }
+
+    public String read() {
+        return repository.read();
+    }
+
+    public String write() {
+        return repository.write();
+    }
+
+    public String delete() {
+        return repository.delete();
+    }
+}
